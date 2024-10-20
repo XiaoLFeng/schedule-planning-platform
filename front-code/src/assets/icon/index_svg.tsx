8c26b93352165f00326909a3d37a096d810c902f
XiaoLFeng <gm@x-lf.cn>
2024-10-20 17:52:12 +0800

    feat(index): 完善了首页内容展示
    基础内容展示完成简易配置

---
+++ front-code/src/assets/icon/index_svg.tsx
@@ -0,0 +1,114 @@
+/*
+ * ***************************************************************************************
+ * author: XiaoLFeng(https://www.x-lf.com)
+ * about:
+ *   The project contains the source code of com.xlf.schedule.
+ *   All source code for this project is licensed under the MIT open source license.
+ * licenseStatement:
+ *   Copyright (c) 2016-2024 XiaoLFeng. All rights reserved.
+ *   For more information about the MIT license, please view the LICENSE file
+ *     in the project root directory or visit:
+ *   https://opensource.org/license/MIT
+ * disclaimer:
+ *   Since this project is in the model design stage, we are not responsible for any losses
+ *     caused by using this project for commercial purposes.
+ *   If you modify the code and redistribute it, you need to clearly indicate what changes
+ *     you made in the corresponding file.
+ *   If you want to modify it for commercial use, please contact me.
+ * ***************************************************************************************
+ */
+
+const DocumentMagnifyingGlass = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+             className="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"/>
+        </svg>
+    );
+}
+
+const Forward = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+             className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round"
+                  d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z"/>
+        </svg>
+    );
+}
+
+const Users = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+             className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round"
+                  d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"/>
+        </svg>
+    );
+}
+
+const CommandLine = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+             className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round"
+                  d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z"/>
+        </svg>
+    );
+}
+
+const Adjustments = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+             className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round"
+                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"/>
+        </svg>
+    );
+}
+
+const Eye = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+             className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"/>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+        </svg>
+    );
+}
+
+const Github = () => {
+    return (
+        <svg className="size-6" fill="currentColor" viewBox="0 0 24 24"
+             aria-hidden="true">
+            <path
+                fillRule="evenodd"
+                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+
+const Book = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+             className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round"
+                  d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"/>
+        </svg>
+    );
+}
+
+export {
+    DocumentMagnifyingGlass,
+    Forward,
+    Users,
+    CommandLine,
+    Adjustments,
+    Eye,
+    Github,
+    Book
+}
--- front-code/src/pages/home/home_index.tsx
+++ front-code/src/pages/home/home_index.tsx
@@ -18,12 +18,289 @@
  * ***************************************************************************************
  */
 
+import {useSelector} from "react-redux";
+import {WebInfoEntity} from "../../models/entity/web_info_entity.ts";
+import {Link} from "react-router-dom";
+import {
+    Adjustments,
+    Book,
+    CommandLine,
+    DocumentMagnifyingGlass,
+    Eye,
+    Forward,
+    Github,
+    Users
+} from "../../assets/icon/index_svg.tsx";
+import {LogoSVG} from "../../assets/icon/logo_svg.tsx";
+
+import indexStart from "../../assets/images/index-start.webp";
 
 export function HomeIndex() {
+    const webInfo = useSelector((state: { webInfo: WebInfoEntity }) => state.webInfo);
+
+    document.title = `${webInfo.name} - ${webInfo.description}`;
 
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+            <section className={"relative bg-cover bg-center bg-no-repeat " + "bg-[url(index-background.webp)]"}>
+                <div
+                    className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 sm:bg-gradient-to-r"/>
+                <div
+                    className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+                    <div className="max-w-xl text-center sm:text-left">
+                        <h1 className="text-3xl font-extrabold text-sky-300 sm:text-5xl">
+                            {webInfo.name}
+                        </h1>
+                        <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed">
+                            {webInfo.description}
+                        </p>
+                        <div className="mt-8 flex flex-wrap gap-4 text-center">
+                            <Link to="/dashboard/home"
+                                  className="transition block w-full rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-teal-700 focus:outline-none focus:ring active:bg-teal-500 sm:w-auto">
+                                开始使用
+                            </Link>
+                            <Link to="#"
+                                  className="transition block w-full rounded bg-white px-12 py-3 text-sm font-medium text-teal-500 shadow hover:text-teal-600 focus:outline-none focus:ring active:text-teal-800 sm:w-auto">
+                                了解更多
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className={"flex justify-center"}>
+                <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+                    <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
+                        <div className="mx-auto max-w-lg text-center lg:mx-0 lg:text-left">
+                            <h2 className="text-3xl font-bold sm:text-4xl">全面帮助</h2>
+                            <p className="mt-4 text-gray-600">
+                                学生日程规划平台旨在为学生提供一个全面、高效的日程管理工具，帮助他们合理规划学习与生活，提高时间管理能力。
+                                通过该平台，学生可以轻松创建和管理个人日程、添加和查看课程表，同时与好友建立联系，互相查询彼此的空闲时间。
+                                平台具备用户友好的界面和强大的功能，满足学生在学习期间的多样化需求，助力他们实现自我管理和高效学习。
+                            </p>
+                            <Link
+                                to="/dashboard/home"
+                                className="mt-8 inline-block rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-teal-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                            >
+                                现在开始使用
+                            </Link>
+                        </div>
+
+                        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
+                            <div
+                                className="transition block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                                    <DocumentMagnifyingGlass/>
+                                </span>
+                                <h2 className="mt-2 font-bold">更快速</h2>
+                                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                                    更快速地创建和管理个人日程，提高效率。
+                                </p>
+                            </div>
+                            <div
+                                className="transition block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                                    <Forward/>
+                                </span>
+                                <h2 className="mt-2 font-bold">更高效</h2>
+                                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                                    更高效地添加和查看课程表，提高学习效率。
+                                </p>
+                            </div>
+                            <div
+                                className="transition block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                                    <Users/>
+                                </span>
+                                <h2 className="mt-2 font-bold">更便捷</h2>
+                                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                                    更便捷地与好友建立联系，查询彼此的空闲。
+                                </p>
+                            </div>
+                            <div
+                                className="transition block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                                    <CommandLine/>
+                                </span>
+                                <h2 className="mt-2 font-bold">更智能</h2>
+                                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                                    智能化的安排工具，帮助合理规划时间。
+                                </p>
+                            </div>
+                            <div
+                                className="transition block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                                    <Adjustments/>
+                                </span>
+                                <h2 className="mt-2 font-bold">更协作</h2>
+                                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                                    提高团队沟通效率，促进协作。
+                                </p>
+                            </div>
+                            <div
+                                className="transition block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                                    <Eye/>
+                                </span>
+                                <h2 className="mt-2 font-bold">更直观</h2>
+                                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                                    可视化的时间安排，方便查看和协调时间。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center">
+                <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+                    <div className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+                        <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
+                            立刻使用{webInfo.name}
+                        </h2>
+                        <p className="hidden text-gray-500 md:mt-4 md:block">
+                            {webInfo.description}
+                        </p>
+                        <div className="mt-4 md:mt-8">
+                            <Link
+                                to="/dashboard/home"
+                                className="inline-block rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-teal-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                            >
+                                Get Started Today
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+                <img
+                    alt=""
+                    src={indexStart}
+                    className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[60px]"
+                />
+            </section>
+            <footer className="bg-white">
+                <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
+                    <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+                        <div>
+                            <div className="text-teal-600">
+                                <LogoSVG/>
+                            </div>
+                            <p className="mt-4 max-w-xs text-gray-500">
+                                {webInfo.description}
+                            </p>
+                            <ul className="mt-8 flex gap-6">
+                                <li>
+                                    <Link
+                                        to="https://github.com/XiaoLFeng/schedule-planning-platform"
+                                        rel="noreferrer"
+                                        target="_blank"
+                                        className="text-gray-700 transition hover:opacity-75"
+                                    >
+                                        <span className="sr-only">GitHub</span>
+                                        <Github/>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        to="https://schedule-planning-platform.pages.dev/"
+                                        rel="noreferrer"
+                                        target="_blank"
+                                        className="text-gray-700 transition hover:opacity-75"
+                                    >
+                                        <span className="sr-only">Document</span>
+                                        <Book/>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-4">
+                            <div>
+                                <p className="font-medium text-gray-900">服务</p>
+                                <ul className="mt-6 space-y-4 text-sm">
+                                    <li>
+                                        <Link to={""}
+                                              className="text-gray-700 transition hover:opacity-75">课程表</Link>
+                                    </li>
+                                    <li>
+                                        <Link to={""}
+                                              className="text-gray-700 transition hover:opacity-75">日程</Link>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div>
+                                <p className="font-medium text-gray-900">作者</p>
+                                <ul className="mt-6 space-y-4 text-sm">
+                                    <li>
+                                        <Link to={"https://blog.x-lf.com/"} target={"_blank"}
+                                              className="text-gray-700 transition hover:opacity-75">
+                                            博客
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to={"https://www.github.com/XiaoLFeng"} target={"_blank"}
+                                              className="text-gray-700 transition hover:opacity-75">
+                                            Github
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to={"https://space.bilibili.com/244321572"} target={"_blank"}
+                                              className="text-gray-700 transition hover:opacity-75">
+                                            哔哩哔哩
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <p className="font-medium text-gray-900">帮助</p>
+                                <ul className="mt-6 space-y-4 text-sm">
+                                    <li>
+                                        <Link to={"/help/faqs"}
+                                              className="text-gray-700 transition hover:opacity-75">FAQs</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <p className="font-medium text-gray-900">友链</p>
+                                <ul className="mt-6 space-y-4 text-sm">
+                                    <li>
+                                        <Link to={"https://blog.x-lf.com/"} target={"_blank"}
+                                              className="text-gray-700 transition hover:opacity-75">
+                                            凌中的锋雨
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to={"https://img.akass.cn/"} target={"_blank"}
+                                              className="text-gray-700 transition hover:opacity-75">
+                                            晓白图床
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to={"https://tailwindcss.com/"} target={"_blank"}
+                                              className="text-gray-700 transition hover:opacity-75">
+                                            TailwindCSS
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to={"https://ant-design.antgroup.com/"} target={"_blank"}
+                                              className="text-gray-700 transition hover:opacity-75">
+                                            Ant Design
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="text-xs text-gray-500 grid">
+                        <span>{webInfo.copyright} 版权所有.</span>
+                        <Link className={"transition hover:opacity-75"} to={"https://beian.miit.gov.cn/"}
+                              target={"_blank"}>{webInfo.icp}</Link>
+                        <Link className={"transition hover:opacity-75"}
+                              to={"https://beian.mps.gov.cn/#/query/webSearch"}
+                              target={"_blank"}>{webInfo.record}</Link>
+                    </p>
+                </div>
+            </footer>
+        </>
     );
 }
